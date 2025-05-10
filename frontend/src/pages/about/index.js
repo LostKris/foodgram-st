fff35927225a1230f042046475e4101520f69e45
Kriseev Mikhail <mkris.yt+ghy@gmail.com>
2025-05-10 05:08:24 +0300

    фронт

--- frontend/src/pages/about/index.js
+++ frontend/src/pages/about/index.js
@@ -38,10 +38,10 @@ const About = ({ updateOrders, orders }) => {
           </h2>
           <div className={styles.text}>
             <p className={styles.textItem}>
-              Код проекта находится тут - <a href="#" className={styles.textLink}>Github</a>
+              Код проекта находится тут - <a href="https://github.com/LostKris/foodgram-st" className={styles.textLink}>Github</a>
             </p>
             <p className={styles.textItem}>
-              Автор проекта: <a href="#" className={styles.textLink}>Имя Автора</a>
+              Автор проекта: <a href="https://github.com/LostKris/" className={styles.textLink}>Крисеев Михаил</a>
             </p>
           </div>
         </aside>
